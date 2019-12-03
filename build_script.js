0a1aa39583294abb0e29bab5940cf24eae7e35e9
Itay Maman <imaman@users.noreply.github.com>
2019-12-04 00:15:06 +0200

    upload

--- build_script.js
+++ build_script.js
@@ -1,12 +1,14 @@
 const { spawn } = require('child_process');
 const AWS = require('aws-sdk')
+const fs = require('fs')
+const path = require('path')
 
 var stepfunctions = new AWS.StepFunctions();
 
-function extractToken(processArgv) {
-    const taskToken = '--step-function-task-token'
-    const args = processArgv.slice(2)
-    const index = args.indexOf(taskToken)
+const args = process.argv.slice(2)
+
+function extractToken(flag) {
+    const index = args.indexOf(flag)
     if (index < 0) {
         throw new Error(`Missing flag: ${flag}`)
     }
@@ -23,15 +25,32 @@ function extractToken(processArgv) {
     return token
 }
 
-const taskToken = extractToken(process.argv)
+const taskToken = extractToken('--step-function-task-token')
+const s3Bucket = extractToken('--s3Bucket')
+const s3Prefix = extractToken('--s3Prefix')
+const s3 = new AWS.S3()
+
+const LIMIT = 1000 * 1000 // 1 MB
+
+async function upload(fileToUpload) {
+    let str = fs.readFileSync(fileToUpload, 'utf-8')
+    if (str.length > LIMIT) {
+        str = str.substring(0, LIMIT)
+    }
+
+    const key = s3Prefix + path.extname(fileToUpload) 
+    return s3.putObject({Bucket: s3Bucket, Key: key, Body: str}).promise()
+}
+
 
 new Promise((resolve, reject) => {    
     const child = spawn('npm', ['run', 'test']);
     child.stdout.pipe(process.stdout);
     child.stderr.pipe(process.stderr);    
 
-    child.on('exit', (code) => {
+    child.on('exit', async (code) => {
       console.log(`child process exited with code ${code}`);
+      await Promise.all([upload('/tmp/wix_build_machine.log'), upload('/tmp/wix_build_machine.json')])
       if (code === 0) {
           resolve()
       } else {
