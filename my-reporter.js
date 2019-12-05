60236e8716e66db18a5ce83a6ed13e143e1c39e1
Itay Maman <imaman@users.noreply.github.com>
2019-12-05 21:19:19 +0200

    _3

--- my-reporter.js
+++ my-reporter.js
@@ -4,70 +4,8 @@
 const fs = require('fs')
 
 const Mocha = require('mocha');
-const {
-  EVENT_RUN_BEGIN,
-  EVENT_RUN_END,
-} = Mocha.Runner.constants;
 
 
-const es = {
-    /**
-     * Emitted when {@link Hook} execution begins
-     */
-    EVENT_HOOK_BEGIN: 'hook',
-    /**
-     * Emitted when {@link Hook} execution ends
-     */
-    EVENT_HOOK_END: 'hook end',
-    /**
-     * Emitted when Root {@link Suite} execution begins (all files have been parsed and hooks/tests are ready for execution)
-     */
-    EVENT_RUN_BEGIN: 'start',
-    /**
-     * Emitted when Root {@link Suite} execution has been delayed via `delay` option
-     */
-    EVENT_DELAY_BEGIN: 'waiting',
-    /**
-     * Emitted when delayed Root {@link Suite} execution is triggered by user via `global.run()`
-     */
-    EVENT_DELAY_END: 'ready',
-    /**
-     * Emitted when Root {@link Suite} execution ends
-     */
-    EVENT_RUN_END: 'end',
-    /**
-     * Emitted when {@link Suite} execution begins
-     */
-    EVENT_SUITE_BEGIN: 'suite',
-    /**
-     * Emitted when {@link Suite} execution ends
-     */
-    EVENT_SUITE_END: 'suite end',
-    /**
-     * Emitted when {@link Test} execution begins
-     */
-    EVENT_TEST_BEGIN: 'test',
-    /**
-     * Emitted when {@link Test} execution ends
-     */
-    EVENT_TEST_END: 'test end',
-    /**
-     * Emitted when {@link Test} execution fails
-     */
-    EVENT_TEST_FAIL: 'fail',
-    /**
-     * Emitted when {@link Test} execution succeeds
-     */
-    EVENT_TEST_PASS: 'pass',
-    /**
-     * Emitted when {@link Test} becomes pending
-     */
-    EVENT_TEST_PENDING: 'pending',
-    /**
-     * Emitted when {@link Test} execution has failed, but will retry
-     */
-    EVENT_TEST_RETRY: 'retry'
-}
 
 // this reporter outputs test results, indenting two spaces per suite
 class MyReporter {
@@ -84,7 +22,7 @@ class MyReporter {
     this.currFile = null
     this.map = new Map()
 
-    runner.once(EVENT_RUN_END, () => {
+    runner.once(Mocha.Runner.constants.EVENT_RUN_END, () => {
       const arr = []
       this.map.forEach((v, k) => {
         arr.push({file: k, passes: v})
@@ -92,16 +30,16 @@ class MyReporter {
       fs.writeFileSync(this.outputFile, JSON.stringify(arr), 'utf-8')
     });
 
-    runner.on(es.EVENT_TEST_BEGIN, t => {
+    runner.on(Mocha.Runner.constants.EVENT_TEST_BEGIN, t => {
       const x = this.map.get(t.file)
       if (!x) {
         this.map.set(t.file, true)
       }
     })
-    runner.on(es.EVENT_TEST_FAIL, t => {
+    runner.on(Mocha.Runner.constants.EVENT_TEST_FAIL, t => {
       this.map.set(t.file, false)
     })
-    runner.on(es.EVENT_TEST_PENDING, t => {
+    runner.on(Mocha.Runner.constants.EVENT_TEST_PENDING, t => {
       this.map.set(t.file, false)
     })
   }
