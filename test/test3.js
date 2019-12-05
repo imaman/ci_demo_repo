9da3dc796e94ea750e5389120b2f5f49f0f2e8ed
Itay Maman <imaman@users.noreply.github.com>
2019-12-05 21:36:51 +0200

    test3-8

---
+++ test/test3.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+describe('foo', function() {
+    it('should do addition', () => {
+        assert.equal(4 + 9, 13);
+    });
+    it('should do multiplcation', () => {
+        assert.equal(4 * 9, 36);
+    });
+});
+
