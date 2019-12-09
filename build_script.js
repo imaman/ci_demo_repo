fa5c5487efd6438b44751290e3268de9b846c6f0
Itay Maman <imaman@users.noreply.github.com>
2019-12-09 11:41:22 +0200

    reofrmat

--- build_script.js
+++ build_script.js
@@ -9,21 +9,21 @@ var stepfunctions = new AWS.StepFunctions();
 const args = process.argv.slice(2)
 
 function extractFlag(flag) {
-    const index = args.indexOf(flag)
-    if (index < 0) {
-        throw new Error(`Missing flag: ${flag}`)
-    }
-    
-    if (index == args.length - 1) {
-        throw new Error(`Flag ${flag} was used without a value`)
-    }
-    
-    const token = String(args[index + 1]).trim()
-    if (!token.length) {
-        throw new Error(`Flag ${flag} must pass a non-empty value`)
-    }
+  const index = args.indexOf(flag)
+  if (index < 0) {
+    throw new Error(`Missing flag: ${flag}`)
+  }
 
-    return token
+  if (index == args.length - 1) {
+    throw new Error(`Flag ${flag} was used without a value`)
+  }
+
+  const token = String(args[index + 1]).trim()
+  if (!token.length) {
+    throw new Error(`Flag ${flag} must pass a non-empty value`)
+  }
+
+  return token
 }
 
 
@@ -37,41 +37,41 @@ const s3 = new AWS.S3()
 const LIMIT = 1000 * 1000 // 1 MB
 
 async function upload(fileToUpload) {
-    let str = fs.readFileSync(fileToUpload, 'utf-8')
-    if (str.length > LIMIT) {
-        str = str.substring(0, LIMIT)
-    }
+  let str = fs.readFileSync(fileToUpload, 'utf-8')
+  if (str.length > LIMIT) {
+    str = str.substring(0, LIMIT)
+  }
 
-    const key = s3Prefix + path.extname(fileToUpload) 
-    return s3.putObject({Bucket: s3Bucket, Key: key, Body: str}).promise()
+  const key = s3Prefix + path.extname(fileToUpload)
+  return s3.putObject({ Bucket: s3Bucket, Key: key, Body: str }).promise()
 }
 
-new Promise((resolve, reject) => {    
+new Promise((resolve, reject) => {
   const files = glob.sync("test/**/*.js")
   console.log('files=' + JSON.stringify(files))
   const child = spawn('npm', ['run', 'test']);
   child.stdout.pipe(process.stdout);
-  child.stderr.pipe(process.stderr);    
+  child.stderr.pipe(process.stderr);
 
-    child.on('exit', async (code) => {
-      console.log(`child process exited with code ${code}`);
-      await Promise.all([upload('/tmp/wix_build_machine.log'), upload('/tmp/wix_build_machine.json')])
-      if (code === 0) {
-          resolve()
-      } else {
-          reject(`Exit code=${code}`)
-      }
-    }); 
+  child.on('exit', async (code) => {
+    console.log(`child process exited with code ${code}`);
+    await Promise.all([upload('/tmp/wix_build_machine.log'), upload('/tmp/wix_build_machine.json')])
+    if (code === 0) {
+      resolve()
+    } else {
+      reject(`Exit code=${code}`)
+    }
+  });
 }).then(() => {
-    const params = {
-        output: JSON.stringify({status: "-OK-"}),
-        taskToken
-    };
-    stepfunctions.sendTaskSuccess(params).promise()
+  const params = {
+    output: JSON.stringify({ status: "-OK-" }),
+    taskToken
+  };
+  stepfunctions.sendTaskSuccess(params).promise()
 }).catch(err => {
-    const params = {
-        output: JSON.stringify({status: "-FAILED-", casuse: err}),
-        taskToken
-    };
-    stepfunctions.sendTaskSuccess(params).promise()
+  const params = {
+    output: JSON.stringify({ status: "-FAILED-", casuse: err }),
+    taskToken
+  };
+  stepfunctions.sendTaskSuccess(params).promise()
 })
